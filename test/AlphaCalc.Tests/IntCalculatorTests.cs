d1111dc1932e3689e0768b4e5033eeab45d8d06b
pahoitr <128758881+pahoitr@users.noreply.github.com>
2023-03-24 10:10:57 +0100

    IntCalculator added

---
+++ test/AlphaCalc.Tests/IntCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AlphaCalc.Tests;
+
+public class IntCalculatorTests
+{
+    [Theory]
+    [InlineData(0, 0, 0)]
+    [InlineData(1, 0, 1)]
+    [InlineData(0, 1, 1)]
+    [InlineData(1, 1, 2)]
+    [InlineData(-1, 1, 0)]
+    public void Sum_ReturnsExpected(int first, int second, int expected)
+    {
+        var actual = IntCalculator.Sum(first, second);
+
+        actual.Should().Be(expected);
+    }
+}
