a6e8d0d0e86348c97241fe7396aa3056fff2ee56
pahoitr <128758881+pahoitr@users.noreply.github.com>
2023-03-24 14:11:02 +0100

    Initial version with IntCalculator added

---
+++ src/AlphaCalc/IntCalculator.cs
@@ -0,0 +1,19 @@
+﻿namespace AlphaCalc
+{
+    /// <summary>
+    /// Provides a set of static method for calculation.
+    /// </summary>
+    public static class IntCalculator
+    {
+        /// <summary>
+        /// Calculates sum of two integers.
+        /// </summary>
+        /// <param name="first">First integer.</param>
+        /// <param name="second">Second integer.</param>
+        /// <returns>Sum of <paramref name="first"/> and <paramref name="second"/>.</returns>
+        public static int Sum(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
