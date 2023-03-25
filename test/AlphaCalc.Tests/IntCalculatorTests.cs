e573313e86fe2e5413e0c27239e8738debbc43fb
pahoitr <128758881+pahoitr@users.noreply.github.com>
2023-03-25 04:21:36 +0100

    IntCalculator: multiply function addded

--- test/AlphaCalc.Tests/IntCalculatorTests.cs
+++ test/AlphaCalc.Tests/IntCalculatorTests.cs
@@ -17,4 +17,22 @@ public class IntCalculatorTests
 
         actual.Should().Be(expected);
     }
+
+    [Theory]
+    [InlineData(0, 0, 0)]
+    [InlineData(1, 0, 0)]
+    [InlineData(0, 1, 0)]
+    [InlineData(1, 1, 1)]
+    [InlineData(-1, 1, -1)]
+    [InlineData(1, -1, -1)]
+    [InlineData(1, 2, 2)]
+    [InlineData(2, 1, 2)]
+    [InlineData(3, 4, 12)]
+    [InlineData(5, 10, 50)]
+    public void Multiply_ReturnsExpected(int first, int second, int expected)
+    {
+        var actual = IntCalculator.Multiply(first, second);
+
+        actual.Should().Be(expected);
+    }
 }
