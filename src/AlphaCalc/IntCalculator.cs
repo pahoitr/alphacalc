8a4771e08a2b3dfdee79a7e653b7354878bf6887
pahoitr <128758881+pahoitr@users.noreply.github.com>
2023-03-25 04:23:42 +0100

    IntCalculator: multiply function addded

--- src/AlphaCalc/IntCalculator.cs
+++ src/AlphaCalc/IntCalculator.cs
@@ -6,14 +6,25 @@
     public static class IntCalculator
     {
         /// <summary>
-        /// Calculates sum of two integers.
+        /// Calculates sum of two numbers.
         /// </summary>
-        /// <param name="first">First integer.</param>
-        /// <param name="second">Second integer.</param>
-        /// <returns>Sum of <paramref name="first"/> and <paramref name="second"/>.</returns>
+        /// <param name="first">First number.</param>
+        /// <param name="second">Second number.</param>
+        /// <returns>Sum of <paramref name="first"/> and <paramref name="second"/> numbers.</returns>
         public static int Sum(int first, int second)
         {
             return first + second;
+        }
+
+        /// <summary>
+        /// Calculates product of two numbers.
+        /// </summary>
+        /// <param name="first">First number.</param>
+        /// <param name="second">Second number.</param>
+        /// <returns>Product of <paramref name="first"/> and <paramref name="second"/> numbers.</returns>
+        public static int Multiply(int first, int second)
+        {
+            return first * second;
         }
     }
 }
